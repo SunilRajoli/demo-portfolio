d5f4f884aa2655685c311612c4dcd72aa3c9e6fb
Rajoli Sunil <102010090+SunilRajoli@users.noreply.github.com>
2024-05-23 06:59:54 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rajoli Sunil",
+  title: "Full Stack Developer",
+  email: "rajolisunilkumar@gmail.com",
+  gitHub: "https://github.com/SunilRajoli",
+  instagram: "#",
+  linkedIn: "https://www.linkedin.com/in/rajoli-sunil-731478212",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "#",
+  youTube: "#",
 };
 
 const primaryColor = "#4E567E";
